7e28afb3c5a491618b60d9917ff5d6228b224700
Mt.Salt <juntaro.shioyama@gmail.com>
2022-05-03 17:26:40 +0900

    change UI details

--- static/js/main.js
+++ static/js/main.js
@@ -58,31 +58,3 @@ function postForgotpasswordData() {
     
     // postRequest(endpointUrl, postData);
 }
-
-function postRequest(endpointUrl, objData) {
-
-    // 通信実行
-    $.ajax({
-        type: "post",
-        url: endpointUrl,
-        data:JSON.stringify(objData),
-        contentType: 'application/json',
-        dataType: "json",
-        success: function(jsonData) {   // 200 OK時
-            // JSON Arrayの先頭が成功フラグ、失敗の場合2番目がエラーメッセージ
-            if (!jsonData[0]) {    // サーバが失敗を返した場合
-                alert("Transaction error. " + json_data[1]);
-                return;
-            }
-            console.log("postRequest succeeded.");
-            location.reload();
-        },
-        error: function() {
-            console.log("error, postRequest failed.");
-        },
-        complete: function() {
-            console.log("postRequest finished.");
-        }
-    });
-
-}
