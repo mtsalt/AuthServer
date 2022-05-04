cefac77c205673a5df7100dced8db917df916831
Mt.Salt <juntaro.shioyama@gmail.com>
2022-05-04 23:18:51 +0900

    refactoring

--- static/js/main.js
+++ static/js/main.js
@@ -2,53 +2,34 @@ let endpointUrl = location.href;
 
 function postRegisterData() {
 
-    let username = getInputElementValue("username");
-    let email = getInputElementValue("email");
-    let password = getInputElementValue("password");
-    
     let postData = {
-        "username": username,
-        "email": email,
-        "password": password
+        "username": getInputElementValue("username"),
+        "email": getInputElementValue("email"),
+        "password": getInputElementValue("password")
     };
 
+    // add result check and inform 
     // postRequest(endpointUrl, postData);
 }
 
 function postSigninData() {
 
-    let username = getInputElementValue("username");
-    let password = getInputElementValue("password");
-
-    try {
-
-    } catch(e) {
-        // set error info to UI.
-    }
-
     let postData = {
-        "username": username,
-        "password": password
+        "username": getInputElementValue("username"),
+        "password": getInputElementValue("password")
     };
 
+    // add result check and inform
     // postRequest(endpointUrl, postData);
 }
 
 function postForgotpasswordData() {
 
-    let email = getInputElementValue("email");
-
-    try {
-
-    } catch(e) {
-        // set error info to UI.
-    }
-
     let postData = {
-        "username": username,
-        "password": password
+        "email": getInputElementValue("email")
     };
-    
+
+    // add result check and inform   
     // postRequest(endpointUrl, postData);
 }
 
@@ -70,7 +51,7 @@ if (emailInputElem != null) {
     });
 }
 
-// event listener (input email)
+// event listener (input password)
 let passwordInputElem = document.getElementById("password");
 if (passwordInputElem != null) {
     passwordInputElem.addEventListener('focusout', (e) => {
